bd9ddabc43e0249c009e092d713a6b0ecbdb253a
Raunaq Singh <rauni2000@gmail.com>
2023-02-10 12:39:25 -0400

    Test2

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-#variable "project" {}
+variable "project" {}
 
-#variable "credentials" {}
+variable "credentials" {}
 
-#variable "region" {
+variable "region" {
 #  default = "us-central1"
-#}
+}
 
-#variable "zone" {
+variable "zone" {
 #  default = "us-central1-c"
-#}
+}
 
-#variable "db_name" {
+variable "db_name" {
 #  default = "demo_postgresPaaS_db"
-#}
+}
 
 
