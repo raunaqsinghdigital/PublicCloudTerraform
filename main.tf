e5d2d159b63778d4ce807af2337c4086ffaa1887
Raunaq Singh <rauni2000@gmail.com>
2023-02-10 12:16:23 -0400

    Updated main.tf and added variables.tf and vpc.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,27 @@
 # The configuration for the `remote` backend.
 terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "privateorg"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "privateorg"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "GitHub-PublicCloudTerraform"
-         }
-       }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "GitHub-PublicCloudTerraform"
+    }
+  }
 
-       required_providers {
+  required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">=4.19"
     }
   }
-     }
+}
+
+provider "google" {
+  credentials = var.credentials
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project" {}
+
+variable "credentials" {}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "db_name" {
+  default = "demo_postgresPaaS_db"
+}
+
+
